0ec4fd613e30b253455401a6b7d7d9456a560321
Akram Shaikh <shaikhakram981971@gmail.com>
2023-06-07 22:08:51 +0530

    initaial commit

---
+++ sum.java
@@ -0,0 +1,10 @@
+package Gitprac;
+
+class Sum{
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 45;
+        int c = a+b;
+        System.out.println("Sum: " + c);
+    }
+}
