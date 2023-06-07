589b29ec09e10c29e49e6b8d2d67fc20605759e8
Akram Shaikh <shaikhakram981971@gmail.com>
2023-06-08 01:02:56 +0530

    Adding multiply.java

---
+++ multiply.java
@@ -0,0 +1,10 @@
+package Gitprac;
+
+public class multiply {
+   public static void main(String[] args) {
+    int x = 45;
+    int y = 45;
+    
+    System.out.println("This is multiply: " + (x*y));
+   }
+}
