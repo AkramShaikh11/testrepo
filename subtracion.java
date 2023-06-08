80f8f261d5dd8d25ec5b82351362f1a232a31d16
Akram Shaikh <shaikhakram981971@gmail.com>
2023-06-08 17:35:09 +0530

    Adding subtraction.java

---
+++ subtracion.java
@@ -0,0 +1,10 @@
+package Gitprac;
+
+public class subtracion {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 45;
+        int c = a-b;
+        System.out.println("This is Sum: " + c);
+    }
+}
